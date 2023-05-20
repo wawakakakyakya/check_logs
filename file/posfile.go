a1eb32602d904a689f074240f9b38d268d3002b6
boiledegg <wawakakakyakya@yahoo.co.jp>
2023-05-20 22:31:52 +0900

    refs #send_mail_per_file logs when updated

--- file/posfile.go
+++ file/posfile.go
@@ -2,22 +2,25 @@ package file
 
 import (
 	"os"
+	"time"
 
 	logger "github.com/wawakakakyakya/GolangLogger"
 	"github.com/wawakakakyakya/configloader/json"
 )
 
 type PosFile struct {
-	Inode    uint64 `json:"inode"`
-	LastLine int64  `json:"last_line"`
-	Path     string `json:"path"`
-	FileName string
-	logger   *logger.Logger
+	Inode     uint64 `json:"inode"`
+	LastLine  int64  `json:"last_line"`
+	Path      string `json:"path"`
+	FileName  string
+	UpdatedAt string `json:"updated_at"`
+	logger    *logger.Logger
 }
 
 func (p *PosFile) Write(inode uint64, lastLine int64) error {
 	p.Inode = inode
 	p.LastLine = lastLine
+	p.UpdatedAt = time.Now().Format("2006-01-02T15:04:05")
 	return json.Write(p.FileName, p)
 }
 
